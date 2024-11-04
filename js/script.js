fd2151d6f91a51ec275133fe9283ec001be901fb
zHikiz <ygobrhamirez@gmail.com>
2024-11-04 01:26:56 -0300

    site prepronto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-// alert ("buenas");
+/* ====== turns the menu hamburger into a X ====== */
+const menuHamburger = document.querySelector('.menu-hamburger');
+menuHamburger.addEventListener('click', (e) => {
+    toggleMenu();
+});
+
+function toggleMenu () {
+    const nav = document.querySelector ('.nav-responsive');
+    menuHamburger.classList.toggle ('charge');
+
+    if (menuHamburger.classList.contains ('charge')) {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+}
